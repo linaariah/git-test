2441a0ecdfcef97fc1efb89ddd668c5b375724c5
Naariah <linaariah@126.com>
2022-05-19 15:51:25 +0800

    push commit

--- src/main/java/com/Naariah/git/GitTest.java
+++ src/main/java/com/Naariah/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("main test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
