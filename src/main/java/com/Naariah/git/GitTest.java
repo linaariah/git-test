981d69d42f10c3e693c45975c3a3ae696091f560
Naariah <linaariah@126.com>
2022-05-20 12:07:46 +0800

    hot-fix commit 5.20 ssh test!

--- src/main/java/com/Naariah/git/GitTest.java
+++ src/main/java/com/Naariah/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hot-fix pull3");
         System.out.println("hot-fix pull4");
         System.out.println("hot-fix pull 5.20");
+        System.out.println("hot-fix pull ssh");
     }
 }
